17b8e6f3a93d43c6e435c8bc9067f2284dfbc0f5
RamingC <tongraming@gmail.com>
2023-04-26 16:27:05 +0700

    laster

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ app.use(cookieParser())
 app.use('/', express.static(path.join(__dirname,'public')))
 
 app.use('/api/auth',require('./src/routes/authRoutes'))
+app.use('/api/users',require('./src/routes/userRoutes'))
 
 
 
